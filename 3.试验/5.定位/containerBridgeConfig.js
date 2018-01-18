4051f640e5ace7349a51f9363640a02a01b9c85b
unknown <114233763@qq.com>
2018-01-18 18:33:37 +0800

    reference version update

--- 3.试验/5.定位/containerBridgeConfig.js
+++ 3.试验/5.定位/containerBridgeConfig.js
@@ -1,7 +1,7 @@
 ﻿var ContainerBridgeConfig = {
     debug: true,
     wechatJsUrl: "http://res.wx.qq.com/open/js/jweixin-1.0.0.js",
-    wechatSignatureApiUrl: "http://mp.caiyunlife.com/wechat/AdvanceApi/JsApiSignature",
+    wechatSignatureApiUrl: "http://km.wx.uxiang.cn/AdvanceApi/MerchantJsApiSignature",
     wechatAppID: "wxaa3ce740e41cf90f",
     wechatAppSecret: "4a77817827222a619eb788e1bd794eb5",
     wechatAPIList: [
@@ -42,7 +42,7 @@
         "openCard"
     ],
     alipayJsUrl: "http://static.alipay.com/aliBridge/1.0.0/aliBridge.min.js",
-    relJsUrl: "https://imapi.icbc.com.cn/icbc/jsrel1.0.6.js",
+    relJsUrl: "https://imapi.icbc.com.cn/icbc/jsrel1.0.7.js",
     relAppID: "",
-    relSignatureApiUrl: "http://u.yibalian.cn/Center/JsApiSignature"//"http://center.elink.yibalian.cn/api/B2EF3A08C1624FF5A78768DD49461C91/JsApi/Signature"
+    relSignatureApiUrl: "http://center.elink.yibalian.cn/api/B2EF3A08C1624FF5A78768DD49461C91/JsApi/Signature"
 };
