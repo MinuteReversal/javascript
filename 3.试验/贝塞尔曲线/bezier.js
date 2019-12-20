ff121d1b30afce4c588dc502e5fbd2969861f175
MinuteReversal <114233763@qq.com>
2019-12-20 16:12:32 +0800

    bezier change inner variable name

--- 3.试验/贝塞尔曲线/bezier.js
+++ 3.试验/贝塞尔曲线/bezier.js
@@ -3,53 +3,60 @@
  * @param {number} num
  */
 function factorial(num) {
-  if (num <= 1) {
-    return 1;
-  } else {
-    return num * factorial(num - 1);
-  }
+    if (num <= 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
 }
 
 /**
  * 贝塞尔
  */
 class Bezier {
-  /**
-   * @param {Array<{x:number,y:number}>}  ctrlPoints
-   */
-  constructor(ctrlPoints = []) {
-    this.ctrlPoints = ctrlPoints;
-  }
-  /**
-   * 公式
-   * @param {number} t 0-1
-   */
-  point(t) {
-    let { ctrlPoints } = this,
-      x = 0,
-      y = 0,
-      n = ctrlPoints.length - 1;
+    controlPoints = [];
+    /**
+     * @param {Array<{x:number,y:number}>} controlPoints
+     */
+    constructor(controlPoints = []) {
+        this.controlPoints = controlPoints;
+    }
+    /**
+     * 公式
+     * @param {number} percent 0-1
+     */
+    point(percent) {
+        let { controlPoints } = this,
+            x = 0,
+            y = 0,
+            n = controlPoints.length - 1;
 
-    ctrlPoints.forEach((item, index) => {
-      if (!index) {
-        x += item.x * Math.pow(1 - t, n - index) * Math.pow(t, index);
-        y += item.y * Math.pow(1 - t, n - index) * Math.pow(t, index);
-      } else {
-        x +=
-          (factorial(n) / factorial(index) / factorial(n - index)) *
-          item.x *
-          Math.pow(1 - t, n - index) *
-          Math.pow(t, index);
-        y +=
-          (factorial(n) / factorial(index) / factorial(n - index)) *
-          item.y *
-          Math.pow(1 - t, n - index) *
-          Math.pow(t, index);
-      }
-    });
-    return {
-      x,
-      y
-    };
-  }
+        controlPoints.forEach((item, index) => {
+            if (!index) {
+                x +=
+                    item.x *
+                    Math.pow(1 - percent, n - index) *
+                    Math.pow(percent, index);
+                y +=
+                    item.y *
+                    Math.pow(1 - percent, n - index) *
+                    Math.pow(percent, index);
+            } else {
+                x +=
+                    (factorial(n) / factorial(index) / factorial(n - index)) *
+                    item.x *
+                    Math.pow(1 - percent, n - index) *
+                    Math.pow(percent, index);
+                y +=
+                    (factorial(n) / factorial(index) / factorial(n - index)) *
+                    item.y *
+                    Math.pow(1 - percent, n - index) *
+                    Math.pow(percent, index);
+            }
+        });
+        return {
+            x,
+            y
+        };
+    }
 }
