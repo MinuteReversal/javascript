8b8f0e73ee3a353a94b7deed1cbbb34e0db98b84
MinuteReversal <114233763@qq.com>
2020-11-12 15:43:20 +0800

    sleep

--- 3.试验/sleep/worker.js
+++ 3.试验/sleep/worker.js
@@ -1,7 +1,5 @@
-function sleep(milliseconds) {
-    let now = Date.now();
-    while (Date.now() - now < milliseconds);
-}
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
+importScripts("sleep.js");
 
 addEventListener("message", (evt) => {
     sleep(5 * 1000);
