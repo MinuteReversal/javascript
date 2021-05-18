f98c9173970e7fdee5e72ae6c1f3cd1c566b2960
MinuteReversal <114233763@qq.com>
2021-05-18 11:05:41 +0800

    web gl

---
+++ 3.试验/matrixgl/shader_tool.js
@@ -0,0 +1,52 @@
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebGLShader
+ */
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+
+  gl.shaderSource(shader, source);
+
+  // Compile the shader program
+
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    gl.deleteShader(shader);
+    throw new Error(
+      "An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader)
+    );
+  }
+
+  return shader;
+}
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
+ */
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    throw new Error(
+      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+        shaderProgram
+      )}`
+    );
+  }
+
+  return shaderProgram;
+}
